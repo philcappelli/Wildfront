4b8123445e2083ff5d49a5820a92a27ce21d0178
Phil Cappelli <pcappelli901@gmail.com>
2023-10-05 15:45:33 -0400

    Initial Commit

---
+++ Wildfront/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Wildfront
+//
+//  Created by Philip on 10/5/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Wildfront/WildfrontApp.swift
@@ -0,0 +1,17 @@
+//
+//  WildfrontApp.swift
+//  Wildfront
+//
+//  Created by Philip on 10/5/23.
+//
+
+import SwiftUI
+
+@main
+struct WildfrontApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
